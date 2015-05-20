35cfcc44b5f2d485960d8f684d47e10ceea27941
Eidan Kleinamn <eidankl@gmail.com>
2015-05-20 23:07:10 +0300

    second commit

--- ex_02_WS_eidanKleinman/CD/index.js
+++ ex_02_WS_eidanKleinman/CD/index.js
@@ -1,12 +1,20 @@
 var CDJson = require('./stock.json');
 
 
-/* CDStock hold the cd at the json */
+/* CDStock for Json data  */
 function CDStock(){
-	CDJson;
+	this.cds = [];	//declaring an array that hold JSON Array data
+	
+	for(var i = 0; i < CDJson.CDStock.length ; i++){
+		this.cds.push({"Name" : CDJson.CDStock[i].Name,
+					   "ID" : CDJson.CDStock[i].ID,
+					   "Month" : CDJson.CDStock[i].Month,
+			 		   "Price" : CDJson.CDStock[i].Price});
+	}
 }
 
-/* getCD: return all the CD that sold, No param */
+/* getCD: return all the CD that sold, No param, return type Json
+	If there is no sold CD print "There is no CD" */
 CDStock.prototype.getCD = function(){
 	console.log("\ngetCD() function called")
 	var length = CDJson.CDStock.length;
@@ -16,83 +24,82 @@ CDStock.prototype.getCD = function(){
 			return 0;
 	}
 
-	/*print the json */
+	/*print the json to console */
 	console.log("Sold CD:")
 	for(var i = 0; i < CDJson.CDStock.length; i++){
 		console.log("**************");
-		console.log("ID:" + CDJson.CDStock[i].ID);
-		console.log("Name:" + CDJson.CDStock[i].Name);
-		console.log("Date:" + CDJson.CDStock[i].Date);
-		console.log("Price:" + CDJson.CDStock[i].Price);
+		console.log("ID: " + CDJson.CDStock[i].ID);
+		console.log("Name: " + CDJson.CDStock[i].Name);
+		console.log("Month: " + CDJson.CDStock[i].Month);
+		console.log("Price: " + CDJson.CDStock[i].Price);
 	}
 	return CDJson;
 }
 
-/* getCDByMonth: return all the CD that sold at specific month, 1 Param-->Month */
+/* getCDByMonth: return all the CD that sold at specific month,
+ 1 Param-->Month , return type Json
+ if there is no matching print "NO match --> There is no CD that sold this month(month)"  */
 CDStock.prototype.getCDByMonth = function(month){
-	var found = 0;//if the month found this flag will be 1
-	var stack = {
-		CDStock:[]
-	};//array for matching CD by  month
-
 	console.log("\n\ngetCDByMonth(" + month + ") called:")
+
+	var found = 0;//if the month found this flag will be 1
+	var CDStock = []; //array for matching CD by  month
+
 	for(var i = 0; i < CDJson.CDStock.length; i++){
-		if(CDJson.CDStock[i].Date == month){
+		if(CDJson.CDStock[i].Month == month){
 				found = 1;
 				
 				console.log("**************");
-				console.log("ID:" + CDJson.CDStock[i].ID);
-				console.log("Name:" + CDJson.CDStock[i].Name);
-				console.log("Date:" + CDJson.CDStock[i].Date);
-				console.log("Price:" + CDJson.CDStock[i].Price);
+				console.log("ID: " + CDJson.CDStock[i].ID);
+				console.log("Name: " + CDJson.CDStock[i].Name);
+				console.log("Month: " + CDJson.CDStock[i].Month);
+				console.log("Price: " + CDJson.CDStock[i].Price);
 				
-				 stack.CDStock.push({
+				CDStock.push(this.cds[i]);
 				
-					"ID" : CDJson.CDStock[i].ID,
-					"Name" : CDJson.CDStock[i].Name,
-					"Date" : CDJson.CDStock[i].Date,
-					"Price" : CDJson.CDStock[i].Price
-				});
-				console.log(stack.CDStack.pop());
-
-				//stack.push(CDJson.CDStock[i]);
-					
 		}
 		
 	}
 	/* if there is no match for month --> print no match*/
 	if(found == 0){
 		console.log("NO match --> There is no CD that sold this month (" + month + ")" );
-		return 0;
+		return {};
 	}
-	return stack;
+	return CDStock;
+	//return stack.CDStock;
 }
 
-/* getCDByMonthAndName: return all the CD that sold at specific month with specific name, 2 Param-->Month & Name, Return val-->stack with CD obj */
+/* getCDByMonthAndName: return all the CD that sold at specific month with specific name,
+ 2 Param-->Month & Name, Return val-->stack with CD obj
+ if there is no match print NO match --> There is no CD that sold this month(month)  with the name:(name)*/
 CDStock.prototype.getCDByMonthAndName = function(month,name){
-	var found = 0;//if the month and name found this flag will be 1
-	var stack = [];//array for matching cd by  month and name
-
 	console.log("\n\ngetCDByMonthAndName(" + month + "," + name + ") called:")
+
+	var found = 0;//if the monthAndName found this flag will be 1
+	var CDStock = []; //array for matching CD by  monthAndName
+
+	
 	for(var i = 0; i < CDJson.CDStock.length; i++){
-		if(CDJson.CDStock[i].Date == month && CDJson.CDStock[i].Name == name){
+		if(CDJson.CDStock[i].Month == month && CDJson.CDStock[i].Name == name){
 				found = 1;
 				console.log("**************");
-				console.log("ID:" + CDJson.CDStock[i].ID);
-				console.log("Name:" + CDJson.CDStock[i].Name);
-				console.log("Date:" + CDJson.CDStock[i].Date);
-				console.log("Price:" + CDJson.CDStock[i].Price);
+				console.log("ID: " + CDJson.CDStock[i].ID);
+				console.log("Name: " + CDJson.CDStock[i].Name);
+				console.log("Month: " + CDJson.CDStock[i].Month);
+				console.log("Price: " + CDJson.CDStock[i].Price);
 				
-				stack.push(CDJson.CDStock[i]);
+				CDStock.push(this.cds[i]);
+				
+
 		}
 
 	}
 	/* if there is no match for month and name --> print no match*/
 	if(found == 0){
 		console.log(" NO match --> There is no CD that sold this month (" + month + ")" +" with the name:" + name );
-		return 0;
+		return {};
 	}
-	return stack;
+	return CDStock;
 }
 
 
--- ex_02_WS_eidanKleinman/ws_index.js
+++ ex_02_WS_eidanKleinman/ws_index.js
@@ -6,38 +6,40 @@ var CDStock = require('./CD');		//ref to my module
 var CD = new CDStock;				//new instance from CD
 
 /*  ROOT */
+/* http://localhost:3000/ */ 
 app.get('/', function(req,res){
+	res.set('X-header_One', 'app.get');
 	res.send(" localhost called, CDStock WS ROOT");
-	console.log("localhost called, CDStock WS ROOT");
+	
 });
 
 /* getCD */
-app.get('/getCD', function(req,res){
+/* http://localhost:3000/cd */
+app.get('/cd', function(req,res){
 	var temp = CD.getCD();
+	res.set('X-header_One', 'app.getCD');
 	res.json(temp);
 	
 });
 
 /* getCDByMonth*/
-app.get('/getCDByMonth', function(req,res) {
+/* http://localhost:3000/month?Month=April */ 
+app.get('/month', function(req,res) {
 	var urlPart = url.parse(req.url,true);
  	var query = urlPart.query;
- 	//res.json('PPPPPP :' + query.Date);
-	//var urlPart = url.parse(req.url,true);
-	//var query = CD.getCDByMonth(query.month);
-	//res.json(id + " id");
-	//var urlObj = url.parse(req.url,true);
-	//var query = urlObj.query;
-	var temp = CD.getCDByMonth(query.Date);
-	res.json(temp + "hhhhhhh");
-	//res.status(200).json(CD.getCDByMonth(req.params.Date)  + "temp");
+	var temp = CD.getCDByMonth(query.Month);
+	res.set('X-header_One', 'app.getCDByMonth');
+	res.json(temp);
+	
 });
 
-/* ngetCDByMonthAndName */
-app.get('/getCDByMonthAndName', function(req,res){
+/* getCDByMonthAndName */
+/* http://localhost:3000/monthAndName?Month=June&&Name=John%20Lenon */ 
+app.get('/monthAndName', function(req,res){
 	var urlObj = url.parse(req.url,true);
 	var query = urlObj.query;
-	var temp = CD.getCDByMonthAndName(parseInt(query.month),parseInt(query.name));
+	var temp = CD.getCDByMonthAndName((query.Month),(query.Name));
+	res.set('X-header_One', 'app.getCDByMonth');
 	res.json(temp);
 })
 
